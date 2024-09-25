92524fb95129e736bffd801f8cbd20e735726fea
R-a-wan <rawanmariamraneem@gmail.com>
2024-09-25 15:46:31 -0700

    Update version1.0.1.cpp

--- version1.0.1.cpp
+++ version1.0.1.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 #include <stdlib.h>
 #include <random>
+#include "needed.cpp"
 using namespace std;
 random_device rd;
 mt19937 gen(rd());
@@ -13,72 +14,12 @@ int random(int low, int high)
     uniform_int_distribution<> dist(low, high);
     return dist(gen);
 }
-int checkwin()
-{
-    if(square[1] == square[2] && square[2]== square[3])
-    {
-        return 1;
-    }
-    else if(square[4] == square[5] && square[5]== square[6])
-    {
-        return 1;
-    }
-    else if(square[7] == square[8] && square[8]== square[9])
-    {
-        return 1;
-    }
-    else if(square[1] == square[4] && square[4]== square[7])
-    {
-        return 1;
-    }
-    else if(square[2] == square[5] && square[5]== square[8])
-    {
-        return 1;
-    }
-    else if(square[3] == square[6] && square[6]== square[9])
-    {
-        return 1;
-    }
-    else if(square[1] == square[5] && square[5]== square[9])
-    {
-        return 1;
-    }
-    else if(square[3] == square[5] && square[5]== square[7])
-    {
-        return 1;
-    }
-    else if(square[1] != '1' && square[2]!= '2' && square[3]!= '3' && square[4]!= '4' && square[5]!= '5' && square[6]!= '6' && square[7]!= '7' && square[8]!= '8' && square[9]!= '9')
-    {
-        return 0;
-    }
-    else{
-        return -1;
-    }
-}
 
 /*
 system("cls");
 It is used to pass the commands that can be executed in the command processor or the terminal of the operating system,
  and finally returns the command after it has been completed
 */
-void board()
-{
-    system("cls");
-    cout<<"\n\n Tic Tac Toe game \n\n";
-    cout<<"Player 1(X) Player 2(O)";
-
-    cout<<"   |   |   "<<endl;
-    cout<<square[1]<<" | "<<square[2]<<" | "<<square[3]<<endl;
-    cout<<"___|___|___"<<endl;
-    cout<<"   |   |   "<<endl;
-    cout<<square[4]<<" | "<<square[5]<<" | "<<square[6]<<endl;
-    cout<<"___|___|___"<<endl;
-    cout<<"   |   |   "<<endl;
-    cout<<square[7]<<" | "<<square[8]<<" | "<<square[9]<<endl;
-    cout<<"___|___|___"<<endl;
-    cout<<"   |   |   "<<endl;
-
-}
 
 int main()
 {
@@ -141,6 +82,7 @@ int main()
     }
     else{
         cout<<"computer game\n";
+        do{
         choice = random(1,9);
         board();
         if(choice == 1 && square[1]=='1')
@@ -180,7 +122,7 @@ int main()
             square[9]= mark;
         }
     i = checkwin();
-      
+        }while(i != -1);
     }
 
     return 0;
