a2a82fcb918f71359f48027eb6b12f9fd69b1a72
R-a-wan <rawanmariamraneem@gmail.com>
2024-09-25 16:25:21 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,3 +9,21 @@ int row, column;
 char turn = 'X';
 bool draw = false;
 
+void dis_board()
+{
+    system("cls");
+    cout<<"\n\n Tic Tac Toe game \n\n";
+    cout<<"Player 1(X) Player 2(O)";
+
+    cout<<"   |   |   "<<endl;
+    cout<<board[0][0]<<" | "<<board[0][1]<<" | "<<board[0][2]<<endl;
+    cout<<"___|___|___"<<endl;
+    cout<<"   |   |   "<<endl;
+    cout<<board[1][0]<<" | "<<board[1][1]<<" | "<<board[1][2]<<endl;
+    cout<<"___|___|___"<<endl;
+    cout<<"   |   |   "<<endl;
+    cout<<board[2][0]<<" | "<<board[2][1]<<" | "<<board[2][2]<<endl;
+    cout<<"___|___|___"<<endl;
+    cout<<"   |   |   "<<endl;
+
+}
