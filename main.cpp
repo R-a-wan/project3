84117199345f8a5f1e8064d58cc5052adeec2001
R-a-wan <rawanmariamraneem@gmail.com>
2024-09-27 05:56:07 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,21 +9,29 @@ int row, column;
 char turn = 'X';
 bool draw = false;
 
-void dis_board()
-{
-    system("cls");
-    cout<<"\n\n Tic Tac Toe game \n\n";
-    cout<<"Player 1(X) Player 2(O)";
-
-    cout<<"   |   |   "<<endl;
-    cout<<board[0][0]<<" | "<<board[0][1]<<" | "<<board[0][2]<<endl;
-    cout<<"___|___|___"<<endl;
-    cout<<"   |   |   "<<endl;
-    cout<<board[1][0]<<" | "<<board[1][1]<<" | "<<board[1][2]<<endl;
-    cout<<"___|___|___"<<endl;
-    cout<<"   |   |   "<<endl;
-    cout<<board[2][0]<<" | "<<board[2][1]<<" | "<<board[2][2]<<endl;
-    cout<<"___|___|___"<<endl;
-    cout<<"   |   |   "<<endl;
-
+int main(){
+    cout<<"TIC-TACK-TOE\n";
+    cout<<"For two players\n";
+    while(game_over()){
+         display_board();
+         player_turn();
+         game_over();
+        
+    }
+    if(turn =='X' && draw == false){
+        cout<<"Congratulations! Player with 'X' has won";
+    }
+    else if(turn =='O' && draw == false){
+        cout<<"Congratulations! Player with 'X' has won";
+    }
+    else if(draw == true){
+        cout<<"Computer turn\n";
+        display_board();
+         while(game_over()){
+         display_board();
+         player_turn();
+         game_over();
+        
+    }
+    }
 }
